7dd60ac338b8d5f9f17b802f082fcb6e819f2bea
TURAN\turan <turan@suslu.eu>
2021-01-29 10:39:07 +0100

    Kleine fix om ervoor te zorgen dat de ballon niet te hoog gaat

--- Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -34,9 +34,13 @@ public class PlayerControllerX : MonoBehaviour
     void Update()
     {
         // While space is pressed and player is low enough, float up
-        if (Input.GetKey(KeyCode.Space) && !gameOver && transform.position.y < 16)
+        if (Input.GetKey(KeyCode.Space) && !gameOver && transform.position.y < 15)
         {
             playerRb.AddForce(Vector3.up * floatForce);
+        }
+        else if (transform.position.y > 15)
+        {
+            playerRb.AddForce(Vector3.down * 1);
         }
     }
 
